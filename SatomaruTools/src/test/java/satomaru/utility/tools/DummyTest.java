33960afed0759537022c6316fbe5032dc1f00322
Satomaru <erynenadan@gmail.com>
2017-03-10 10:36:02 +0900

    JUnitを4.10に変更。

---
+++ SatomaruTools/src/test/java/satomaru/utility/tools/DummyTest.java
@@ -0,0 +1,13 @@
+package satomaru.utility.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void test() throws Exception {
+		assertTrue(true);
+	}
+}
